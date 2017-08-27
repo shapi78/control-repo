103ecf0798d24310c35c2f843d292d8099dbd375
Shachar Shapira <shapi78@gmail.com>
2017-08-27 14:18:28 +0300

    create_resource mssql changing variables

--- manifests/site.pp
+++ manifests/site.pp
@@ -14,8 +14,9 @@ node default {
 	if ( $dbAction ) {
 		include class {'mssql': }
 		$sqldb_jobs = hiera("sql_db")
-		notify { "defaults are  ${automation::params::sql_defaults}": }
-		create_resources(mssql::database, $sqldb_jobs, ${automation::params::sql_defaults})
+		$create_defaults = $automation::params::sql_defaults
+		notify { "defaults are  $create_defaults": }
+		create_resources(mssql::database, $sqldb_jobs, $create_defaults)
 ##		mssql::backup { "test":
 ##				 version => 20,
 ##				}
